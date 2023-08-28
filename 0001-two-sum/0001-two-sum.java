99ae5d728c1146b429280d7fc4d7289f71900768
Atul Banchhor <99315260+atulbanchhor@users.noreply.github.com>
2023-08-28 22:18:31 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0;i< nums.length;i++){
-            for(int j = i+1;j< nums.length;j++){
-                if(nums[i]+nums[j]==target){
+            for(int j = i+1;j< nums.length;j++){            // maine 2 loop chalaya jo 0 position ko pakad k aaage badthe rahega to check karte rahega apne ek aage s 
+                if(nums[i]+nums[j]==target){                // aur jahan bhi dono ka sum target k equal aaya wahin s uski index return kr dega 
                     return new int[]{i,j};
                     
                 }
